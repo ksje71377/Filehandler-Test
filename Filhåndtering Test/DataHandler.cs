a6708d9f3231fc2cdf92d7b16e5f6e21661e4284
ksje713777 <ksje71377@edu.ucl.dk>
2025-04-03 12:40:05 +0200

    Fungerende version

---
+++ Filhåndtering Test/DataHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filhåndtering_Test
+{
+    internal class DataHandler
+    {
+        const string filePath = "cars.txt";
+        public void CreateList()
+        {
+            List<Car> cars = new List<Car>();
+        }
+        public void SaveToFile(string filePath, List<Car> cars)
+        {
+            // Write the car data to the file
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                foreach (Car car in cars)
+                {
+                    writer.WriteLine($"{car.Make},{car.Model},{car.Year},{car.Color}");
+                }
+            }
+        }
+    }
+}
--- Filhåndtering Test/Program.cs
+++ Filhåndtering Test/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Software til at test gemme og læse fra en fil");
+            Car car = new Car();
+            DataHandler dataHandler = new DataHandler();
+            Console.WriteLine("Tast mærke:");
+            car.Make = Console.ReadLine();
+            Console.WriteLine("Tast model:");
+            car.Model = Console.ReadLine();
+            Console.WriteLine("Tast årgang:");
+            car.Year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tast farve:");
+            car.Color = Console.ReadLine();
+            Console.WriteLine("Tryk en tast og så oprettes listen");
+            Console.ReadKey();
+            List<Car> cars = new List<Car>();
+            cars.Add(car);
+            dataHandler.SaveToFile("cars.txt", cars);
+            Console.ReadKey();
         }
     }
 }
